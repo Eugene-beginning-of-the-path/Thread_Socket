0e84c725fe5b3fe8929b4af8a5e3fcc2ad65e9ae
maysw <maysw@LEEXY>
2022-05-28 16:19:36 +0300

    Add project files.

---
+++ Thread_Socket.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
